db4251c78d29f1f7c6de4817ac2a547d57c12874
oscarbasaguren4 <101658531+oscarbasaguren4@users.noreply.github.com>
2023-11-14 16:17:23 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Definir una función que suma dos números
+def sumar_numeros(num1, num2):
+    suma = num1 + num2
+    return suma
+
+# Definir dos variables con números
+numero1 = 10
+numero2 = 5
+
+# Llamar a la función y mostrar la suma usando print
+resultado = sumar_numeros(numero1, numero2)
+print(f"La suma de {numero1} y {numero2} es: {resultado}")
